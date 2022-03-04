8daa9c88363281973ce7a17bc89c4bf5a9d525c2
Önder ERTAN <nigbuk1998@hotmail.com>
2022-03-04 04:30:00 +0300

    Updated

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -53,27 +53,30 @@ namespace RSACrypt
         {                               // relatively prime means that e and z will not have any other common divisor than 1
             Random random = new Random();
             int e;
-        genelabel:
-            e = random.Next(n); //generates positive number that is less than n
-            if (z % e != 0)
-            {
-                return e;
+            for(; ; ) 
+            { 
+                e = random.Next(n); //generates positive number that is less than n
+                if (z % e != 0 && PrimeChk(e))            
+                    return e;            
+                else
+                    continue;
             }
-            else
-                goto genelabel;
-            
+
         }
         public int GenD(int e, int z) //generates d with condition: e*d mod z == 1
         {
             Random random = new Random();
             int d;
-        gendlabel:
+        for(; ; ) 
+            {
 
-            d = random.Next(z, int.MaxValue); //since e*d mod z == 1, d must be greaeter than z so minimum value is z and maximum value is max value of int data type
-            if ((e * d) % z == 1)
-                return d;
-            else
-                goto gendlabel;
+                d = random.Next(z, int.MaxValue); //since e*d mod z == 1, d must be greater than z so minimum value is z and maximum value is max value of int data type
+                if ((e * d) % z == 1)
+                    return d;
+                else
+                    continue;
+
+            }
 
         }
 
@@ -88,11 +91,11 @@ namespace RSACrypt
         private void CalculateKeysButton_Click(object sender, RoutedEventArgs e) //calculates the keys, will generate e if the textbox is empty
         {
             if (String.IsNullOrEmpty(ETextBox.Text))
-                PublicETextbox.Text = ETextBox.Text = GenE(Convert.ToInt32(NTextBox.Text), Convert.ToInt32(ZTextBox.Text)).ToString();
+                PubETextBox.Text = PublicETextbox.Text = ETextBox.Text = GenE(Convert.ToInt32(NTextBox.Text), Convert.ToInt32(ZTextBox.Text)).ToString();
             else
-                PublicETextbox.Text = ETextBox.Text;
-            PublicNTextbox.Text = PrivateNTextbox.Text = NTextBox.Text ;
-            PrivateDTextbox.Text = GenD(Convert.ToInt32(ETextBox.Text), Convert.ToInt32(ZTextBox.Text)).ToString();
+                PubETextBox.Text = PublicETextbox.Text = ETextBox.Text;
+            PublicNTextbox.Text = PubNTextBox.Text = PrivateNTextbox.Text = PrivNTextBox.Text = NTextBox.Text ;
+            PrivateDTextbox.Text = PrivDTextBox.Text = GenD(Convert.ToInt32(ETextBox.Text), Convert.ToInt32(ZTextBox.Text)).ToString();
 
         }
     }
